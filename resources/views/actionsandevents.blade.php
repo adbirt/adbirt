7781f6e018d337ba9c52d08940889e3d010ab26a
danroyal001 <danroyaleffiong@gmail.com>
2022-02-15 18:13:30 +0100

    _

--- resources/views/actionsandevents.blade.php
+++ resources/views/actionsandevents.blade.php
@@ -218,7 +218,7 @@
             <p class="h2 text-black">Monetize Your Website Traffic With Adbirt</p>
         </div>
 
-        <div class="m-4 row">
+        {{-- <div class="m-4 row">
             <div class="col-12 col-md-4 d-flex-flex-row align-items-center justify-content-center text-center">
                 <div>
                     <img src="/public/assets-revamp/img/actions-and-events/register.png" class="steps_image" />
@@ -248,6 +248,47 @@
                     <p class="h3 font-weight-bold">3. Place It On Your Website</p>
                     <p>Place it on your website, Email Leads, Social Media handles,Whatsapp group, Blog or other
                         Channels.</p>
+                </div>
+            </div>
+
+        </div> --}}
+
+        <div class="mx-4">
+            <div class="row">
+                <div class="col d-flex justify-content-center">
+                    <img src="./images/register.png" class="steps_image" />
+                </div>
+                <div class="col d-flex align-items-center">
+                    <div class="px-1 mx-1 px-sm-5 mx-sm-5">
+                        <p class="h3 font-weight-bold">1. Create An Account</p>
+                        <p>Set up your publishers account by telling us more about your medium of monetization.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row my-5 d-flex">
+                <div class="col d-flex align-items-center order-2 order-sm-1">
+                    <div class="px-1 mx-1 px-sm-5 mx-sm-5">
+                        <p class="h3 font-weight-bold">2. Choose An Ad Campaign</p>
+                        <p>Pick a Campaign that is suitable to the interests of your Audience, i.e email leads or website.
+                        </p>
+                    </div>
+                </div>
+                <div class="col d-flex justify-content-start order-1 order-sm-2">
+                    <img src="./images/choose-ad.png" class="steps_image" />
+                </div>
+            </div>
+
+            <div class="row my-5">
+                <div class="col d-flex justify-content-center">
+                    <img src="./images/upload-ad.png" class="steps_image" />
+                </div>
+                <div class="col d-flex align-items-center">
+                    <div class="px-1 mx-1 px-sm-5 mx-sm-5">
+                        <p class="h3 font-weight-bold">3. Place It On Your Website</p>
+                        <p>Place it on your website, Email Leads, Social Media handles,Whatsapp group, Blog or other
+                            Channels.</p>
+                    </div>
                 </div>
             </div>
 

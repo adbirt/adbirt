955f1cb23f521e51749b521f855eb025de36e05b
danroyal001 <danroyaleffiong@gmail.com>
2022-02-15 19:31:00 +0100

    added Open-Sans and Roboto font families

--- resources/views/actionsandevents.blade.php
+++ resources/views/actionsandevents.blade.php
@@ -13,6 +13,10 @@
             font-size: 2rem !important;
         }
 
+        .font-size-15 {
+            font-size: 15px !important;
+        }
+
         .hero_image {
             width: 300px;
             object-fit: cover;
@@ -130,7 +134,7 @@
                         </div>
                         <div class="my-4 d-flex flex-column align-items-center">
                             <p class="h4 mt-3 open-sans-text">Cost Per Action</p>
-                            <p class="mx-0 mx-lg-2 text-center roboto-text text-black">
+                            <p class="mx-0 mx-lg-2 text-center roboto-text font-size-15 text-black">
                                 Promote your products or service online pay your desired amount when you make a sale
                                 or get a new lead to
                                 your business.
@@ -146,7 +150,7 @@
                         </div>
                         <div class="my-4 d-flex flex-column align-items-center">
                             <p class="h4 mt-3 open-sans-text">Cost Per Sales</p>
-                            <p class="mx-0 mx-lg-2 text-center roboto-text text-black">
+                            <p class="mx-0 mx-lg-2 text-center roboto-text font-size-15 text-black">
                                 You're in total control of your ad budget, set the cost within your means and only
                                 pay after a successful
                                 sales or leads.
@@ -162,7 +166,7 @@
                         </div>
                         <div class="my-4 d-flex flex-column align-items-center">
                             <p class="h4 mt-3 open-sans-text">Cost Per Click</p>
-                            <p class="mx-0 mx-lg-2 text-center roboto-text text-black">
+                            <p class="mx-0 mx-lg-2 text-center roboto-text font-size-15 text-black">
                                 Get unlimited impressions to your business for free and only pay for a valid click
                                 when users visit or
                                 land on your websites.
@@ -267,7 +271,8 @@
                 <div class="col-12 col-md-6 d-flex align-items-center">
                     <div class="px-1 mx-1 px-sm-5 mx-sm-5">
                         <p class="h3 font-weight-bold open-sans-text">1. Create An Account</p>
-                        <p class="roboto-text text-black">Set up your publishers account by telling us more about your
+                        <p class="roboto-text font-size-15 text-black">Set up your publishers account by telling us more
+                            about your
                             medium of
                             monetization.</p>
                     </div>
@@ -278,7 +283,8 @@
                 <div class="col-12 col-md-6 d-flex align-items-center order-2 order-sm-1">
                     <div class="px-1 mx-1 px-sm-5 mx-sm-5">
                         <p class="h3 font-weight-bold open-sans-text">2. Choose An Ad Campaign</p>
-                        <p class="roboto-text text-black">Pick a Campaign that is suitable to the interests of your
+                        <p class="roboto-text font-size-15 text-black">Pick a Campaign that is suitable to the interests of
+                            your
                             Audience, i.e
                             email leads or website.
                         </p>
@@ -296,7 +302,7 @@
                 <div class="col-12 col-md-6 d-flex align-items-center">
                     <div class="px-1 mx-1 px-sm-5 mx-sm-5">
                         <p class="h3 font-weight-bold open-sans-text">3. Place It On Your Website</p>
-                        <p class="roboto-text text-black">Place it on your website, Email Leads, Social Media
+                        <p class="roboto-text font-size-15 text-black">Place it on your website, Email Leads, Social Media
                             handles,Whatsapp
                             group, Blog or other
                             Channels.</p>

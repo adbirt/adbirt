e702e48338b9d7a9dbadfe50761726cb8d7afab8
danroyal001 <danroyaleffiong@gmail.com>
2022-02-16 15:13:41 +0100

    _

--- resources/views/actionsandevents.blade.php
+++ resources/views/actionsandevents.blade.php
@@ -361,165 +361,165 @@
 
     {{-- demarcation --}}
 
-    {{-- <div class="adbirt-content clearfix">
-            <div class="adbirt-page-heading adbirt-size-md adbirt-dynamic-bg"
-                style="background-image: url(public/assets-revamp/img/blog/5.jpg); background-size:cover; background-position: center center;">
-                <div class="container">
-                    <div class="adbirt-page-heading-in text-center">
-                        <h1 class="adbirt-page-heading-title">Actions and events</h1>
-                        <div class="adbirt-post-label">
-                            <span><a href="/">Home</a></span>
-                            <span>Actions and events</span>
-                        </div>
+    <div class="adbirt-content clearfix">
+        <div class="adbirt-page-heading adbirt-size-md adbirt-dynamic-bg"
+            style="background-image: url(public/assets-revamp/img/blog/5.jpg); background-size:cover; background-position: center center;">
+            <div class="container">
+                <div class="adbirt-page-heading-in text-center">
+                    <h1 class="adbirt-page-heading-title">Actions and events</h1>
+                    <div class="adbirt-post-label">
+                        <span><a href="/">Home</a></span>
+                        <span>Actions and events</span>
                     </div>
                 </div>
-            </div><!-- adbirt-page-heading -->
+            </div>
+        </div><!-- adbirt-page-heading -->
 
-        </div> --}}
+    </div>
 
     <!-- Begin Adbirt for Advertisers -->
-    {{-- <div class="container my-5">
+    <div class="container my-5">
 
-            <div class="row justify-content-center">
-                <div class="col-lg-7 col-xl-6">
-                    <div class="adbirt-section-title text-center wow zoomIn" data-wow-duration="0.3s"
-                        data-wow-delay="0.1s" data-wow-offset="0">
-                        <p class="text-custom-color">Advertise your Products & Services and only pay when any of these Events
-                            or Actions occur</p>
-                    </div>
+        <div class="row justify-content-center">
+            <div class="col-lg-7 col-xl-6">
+                <div class="adbirt-section-title text-center wow zoomIn" data-wow-duration="0.3s" data-wow-delay="0.1s"
+                    data-wow-offset="0">
+                    <p class="text-custom-color">Advertise your Products & Services and only pay when any of these Events
+                        or Actions occur</p>
                 </div>
-                <!--- END COL -->
             </div>
-            <!--- END ROW -->
+            <!--- END COL -->
+        </div>
+        <!--- END ROW -->
 
-            <br>
+        <br>
 
-            <div class="row m-0 p-0">
+        <div class="row m-0 p-0">
 
-                <div class="adbirt-best-service-slide px-2 m-0 col-12 col-lg-3 col-md-3 py-4">
-                    <div class="adbirt-best-service-img">
-                        <img src="public/assets-revamp/img/misc/download.jpg" alt="Cost per download"
-                            style="height: 100px !important; object-fit: contain;">
-                    </div>
-                    <div class="adbirt-best-service-content">
-                        <h3 class="adbirt-best-service-title">1. Cost Per Download (CPD)</h3>
-                        <p class="adbirt-best-service-description">Cost Per Download (CPD) If you have a piece of eBook
-                            or software that you want to promote, you can create an Ad and promote it across our Ad
-                            network. You set your Cost Per Download (CPD). You will be charged that amount for every
-                            download. NO Download, No Charge from your Account.
-                        </p>
-                    </div>
+            <div class="adbirt-best-service-slide px-2 m-0 col-12 col-lg-3 col-md-3 py-4">
+                <div class="adbirt-best-service-img">
+                    <img src="public/assets-revamp/img/misc/download.jpg" alt="Cost per download"
+                        style="height: 100px !important; object-fit: contain;">
                 </div>
-
-                <div class="adbirt-best-service-slide px-2 m-0 col-12 col-lg-3 col-md-3 py-4">
-                    <div class="adbirt-best-service-img">
-                        <img src="public/assets-revamp/img/misc/cart.png" alt="Cost per sale"
-                            style="height: 100px !important; object-fit: contain;">
-                    </div>
-                    <div class="adbirt-best-service-content">
-                        <h3 class="adbirt-best-service-title">2. Cost per sale</h3>
-                        <p class="adbirt-best-service-description">Cost Per Sales (CPS) If you have a Products that you
-                            want to promote and Sell, you can create an Ad for this Product and add the Cost you are
-                            willing to Pay per Sale. Once it's Live across our Ad Network, users will start seeing it
-                            and click on it to buy. If they buy, we charge you the amount you set. If they don't buy,
-                            your Money remains intact.
-                        </p>
-                    </div>
+                <div class="adbirt-best-service-content">
+                    <h3 class="adbirt-best-service-title">1. Cost Per Download (CPD)</h3>
+                    <p class="adbirt-best-service-description">Cost Per Download (CPD) If you have a piece of eBook
+                        or software that you want to promote, you can create an Ad and promote it across our Ad
+                        network. You set your Cost Per Download (CPD). You will be charged that amount for every
+                        download. NO Download, No Charge from your Account.
+                    </p>
                 </div>
-
-                <div class="adbirt-best-service-slide px-2 m-0 col-12 col-lg-3 col-md-3 py-4">
-                    <div class="adbirt-best-service-img">
-                        <img src="public/assets-revamp/img/misc/signup.png" alt="Sign up"
-                            style="height: 100px !important; object-fit: contain;">
-                    </div>
-                    <div class="adbirt-best-service-content">
-                        <h3 class="adbirt-best-service-title">3. Cost Per Registration (CPR)</h3>
-                        <p class="adbirt-best-service-description">Do you want New Customers to Register on your
-                            Website? Create an Ad and set the amount you wish to pay per Signup. Only Pay when a user
-                            successfully signs up on your site.
-                        </p>
-                    </div>
-                </div>
-
-                <div class="adbirt-best-service-slide px-2 m-0 col-12 col-lg-3 col-md-3 py-4">
-                    <div class="adbirt-best-service-img">
-                        <img src="public/assets-revamp/img/misc/email-lead.png" alt="Cost Per Email Lead"
-                            style="height: 100px !important; object-fit: contain;">
-                    </div>
-                    <div class="adbirt-best-service-content">
-                        <h3 class="adbirt-best-service-title">4. Cost Per Email Lead (CPEL)</h3>
-                        <p class="adbirt-best-service-description">Do you want to capture Emails for your Email list or
-                            you want to capture a certain data through form submmission? Create an Ad for this, set your
-                            cost per submmission and start capturing your data across our Ad networks. Your Account will
-                            only get charged on every successful submmission
-                        </p>
-                    </div>
-                </div>
-
-                <div class="adbirt-best-service-slide px-1 m-0 col-12 col-lg-3 col-md-3 py-4">
-                    <div class="adbirt-best-service-img">
-                        <img src="public/assets-revamp/img/misc/click.png" alt="Cost per click"
-                            style="height: 100px !important; object-fit: contain;">
-                    </div>
-                    <div class=" adbirt-best-service-content">
-                        <h3 class="adbirt-best-service-title">5. Cost Per Click (CPC)</h3>
-                        <p class="adbirt-best-service-description">Do you need Traffic to your Website or Landing Page,
-                            create an Ad and set your Cost Per Click. Anytime users successfully clicks that link to
-                            land on your website, you get charged, else your money remains intact.
-                        </p>
-                    </div>
-                </div>
-
-                <div class="adbirt-best-service-slide px-1 m-0 col-12 col-lg-3 col-md-3 py-4">
-                    <div class="adbirt-best-service-img">
-                        <img src="public/assets-revamp/img/misc/call.png" alt="Sign up"
-                            style="height: 100px !important; object-fit: contain;">
-                    </div>
-                    <div class="adbirt-best-service-content">
-                        <h3 class="adbirt-best-service-title">6. Cost Per Call (Cost Per Call)</h3>
-                        <p class="adbirt-best-service-description">PDo you have an Offer where you need interested
-                            Prospect to Call you for negotiation and more details. Create an Ad about this Offer, set
-                            your Cost Per call and go Live across our Ad network. Once a Customer clicks the button and
-                            call you, you get charged, else your money remains intact. Learn more on integrating the
-                            Call functionality on your website.
-                        </p>
-                    </div>
-                </div>
-
-                <div class="adbirt-best-service-slide px-1 m-0 col-12 col-lg-3 col-md-3 py-4">
-                    <div class="adbirt-best-service-img">
-                        <img src="public/assets-revamp/img/misc/install.png" alt="Sign up"
-                            style="height: 100px !important; object-fit: contain;">
-                    </div>
-                    <div class="adbirt-best-service-content">
-                        <h3 class="adbirt-best-service-title">7. Cost Per Install (Coming Soon)</h3>
-                        <p class="adbirt-best-service-description">Only Pay for Successful Installations.
-                        </p>
-                    </div>
-                </div>
-
-                <div class="adbirt-best-service-slide px-1 m-0 col-12 col-lg-3 col-md-3 py-4">
-                    <div class="adbirt-best-service-img">
-                        <img src="public/assets-revamp/img/misc/recommendation.png" alt="Recommendation"
-                            style="height: 100px !important; object-fit: contain;">
-                    </div>
-                    <div class="adbirt-best-service-content">
-                        <h3 class="adbirt-best-service-title">8. Cost Per Recommendation (Coming Soon)
-                        </h3>
-                        <p class="adbirt-best-service-description">Only Pay for Recommendations.
-                        </p>
-                    </div>
-                </div>
-
             </div>
 
-        </div> --}}
-
-    {{-- <div class="container pt-1 pb-4">
-            <br>
-            <div class="d-flex align-items-center justify-content-center">
-                <a href="/dashboard" class="text-white font-weight-bold bg-primary-color btn">Let's Start</a>
+            <div class="adbirt-best-service-slide px-2 m-0 col-12 col-lg-3 col-md-3 py-4">
+                <div class="adbirt-best-service-img">
+                    <img src="public/assets-revamp/img/misc/cart.png" alt="Cost per sale"
+                        style="height: 100px !important; object-fit: contain;">
+                </div>
+                <div class="adbirt-best-service-content">
+                    <h3 class="adbirt-best-service-title">2. Cost per sale</h3>
+                    <p class="adbirt-best-service-description">Cost Per Sales (CPS) If you have a Products that you
+                        want to promote and Sell, you can create an Ad for this Product and add the Cost you are
+                        willing to Pay per Sale. Once it's Live across our Ad Network, users will start seeing it
+                        and click on it to buy. If they buy, we charge you the amount you set. If they don't buy,
+                        your Money remains intact.
+                    </p>
+                </div>
             </div>
-            <br>
-        </div> --}}
+
+            <div class="adbirt-best-service-slide px-2 m-0 col-12 col-lg-3 col-md-3 py-4">
+                <div class="adbirt-best-service-img">
+                    <img src="public/assets-revamp/img/misc/signup.png" alt="Sign up"
+                        style="height: 100px !important; object-fit: contain;">
+                </div>
+                <div class="adbirt-best-service-content">
+                    <h3 class="adbirt-best-service-title">3. Cost Per Registration (CPR)</h3>
+                    <p class="adbirt-best-service-description">Do you want New Customers to Register on your
+                        Website? Create an Ad and set the amount you wish to pay per Signup. Only Pay when a user
+                        successfully signs up on your site.
+                    </p>
+                </div>
+            </div>
+
+            <div class="adbirt-best-service-slide px-2 m-0 col-12 col-lg-3 col-md-3 py-4">
+                <div class="adbirt-best-service-img">
+                    <img src="public/assets-revamp/img/misc/email-lead.png" alt="Cost Per Email Lead"
+                        style="height: 100px !important; object-fit: contain;">
+                </div>
+                <div class="adbirt-best-service-content">
+                    <h3 class="adbirt-best-service-title">4. Cost Per Email Lead (CPEL)</h3>
+                    <p class="adbirt-best-service-description">Do you want to capture Emails for your Email list or
+                        you want to capture a certain data through form submmission? Create an Ad for this, set your
+                        cost per submmission and start capturing your data across our Ad networks. Your Account will
+                        only get charged on every successful submmission
+                    </p>
+                </div>
+            </div>
+
+            <div class="adbirt-best-service-slide px-1 m-0 col-12 col-lg-3 col-md-3 py-4">
+                <div class="adbirt-best-service-img">
+                    <img src="public/assets-revamp/img/misc/click.png" alt="Cost per click"
+                        style="height: 100px !important; object-fit: contain;">
+                </div>
+                <div class=" adbirt-best-service-content">
+                    <h3 class="adbirt-best-service-title">5. Cost Per Click (CPC)</h3>
+                    <p class="adbirt-best-service-description">Do you need Traffic to your Website or Landing Page,
+                        create an Ad and set your Cost Per Click. Anytime users successfully clicks that link to
+                        land on your website, you get charged, else your money remains intact.
+                    </p>
+                </div>
+            </div>
+
+            <div class="adbirt-best-service-slide px-1 m-0 col-12 col-lg-3 col-md-3 py-4">
+                <div class="adbirt-best-service-img">
+                    <img src="public/assets-revamp/img/misc/call.png" alt="Sign up"
+                        style="height: 100px !important; object-fit: contain;">
+                </div>
+                <div class="adbirt-best-service-content">
+                    <h3 class="adbirt-best-service-title">6. Cost Per Call (Cost Per Call)</h3>
+                    <p class="adbirt-best-service-description">PDo you have an Offer where you need interested
+                        Prospect to Call you for negotiation and more details. Create an Ad about this Offer, set
+                        your Cost Per call and go Live across our Ad network. Once a Customer clicks the button and
+                        call you, you get charged, else your money remains intact. Learn more on integrating the
+                        Call functionality on your website.
+                    </p>
+                </div>
+            </div>
+
+            <div class="adbirt-best-service-slide px-1 m-0 col-12 col-lg-3 col-md-3 py-4">
+                <div class="adbirt-best-service-img">
+                    <img src="public/assets-revamp/img/misc/install.png" alt="Sign up"
+                        style="height: 100px !important; object-fit: contain;">
+                </div>
+                <div class="adbirt-best-service-content">
+                    <h3 class="adbirt-best-service-title">7. Cost Per Install (Coming Soon)</h3>
+                    <p class="adbirt-best-service-description">Only Pay for Successful Installations.
+                    </p>
+                </div>
+            </div>
+
+            <div class="adbirt-best-service-slide px-1 m-0 col-12 col-lg-3 col-md-3 py-4">
+                <div class="adbirt-best-service-img">
+                    <img src="public/assets-revamp/img/misc/recommendation.png" alt="Recommendation"
+                        style="height: 100px !important; object-fit: contain;">
+                </div>
+                <div class="adbirt-best-service-content">
+                    <h3 class="adbirt-best-service-title">8. Cost Per Recommendation (Coming Soon)
+                    </h3>
+                    <p class="adbirt-best-service-description">Only Pay for Recommendations.
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="container pt-1 pb-4">
+        <br>
+        <div class="d-flex align-items-center justify-content-center">
+            <a href="/dashboard" class="text-white font-weight-bold bg-primary-color btn">Let's Start</a>
+        </div>
+        <br>
+    </div>
 @stop

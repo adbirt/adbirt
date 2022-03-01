430b418bc55b9980292c692a843dd3985d9b63f7
danroyal001 <danroyaleffiong@gmail.com>
2022-03-01 12:27:33 +0100

    _

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -587,8 +587,8 @@
                     <div class="px-0 mx-0 px-md-2 mx-md-2 px-lg-4 mx-lg-5">
                         <div class="px-2 mx-0 px-md-2 mx-md-2">
                             <h2 class="h2">
-                                No minimum threshold
-                                is<br /><wbr /> required and there's
+                                No minimum threshold<br /><wbr />
+                                is required and there's
                                 no limit to earnings
                             </h2>
                             <p class="mr-5">

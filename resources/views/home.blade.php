f8b0c99a7f56cf2f839cb21aa31e826522bcc52a
danroyal001 <danroyaleffiong@gmail.com>
2022-05-04 17:59:12 +0100

    _

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.frontend')
 
 @section('style')
-    <style>
+    {{-- <style>
         .btn-danger {
             background-color: #ff7c00;
             border: #ff7c00;
@@ -74,7 +74,7 @@
         @media (min-width: 992px) {
             .hero_image {
                 width: 600px;
-                {{-- image-rendering: pixelated !important; --}}
+                /* image-rendering: pixelated !important; */
             }
 
             .steps_image {
@@ -90,7 +90,18 @@
             }
         }
 
-    </style>
+    </style> --}}
+
+    <link href="{{ asset('/css/style.css') }}" rel="stylesheet">
+
+
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,700,500,900' rel='stylesheet' type='text/css'>
+
+    <link href="{{ asset('/css/magnific-popup.css') }}" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/jquery.fancybox.css" media="screen" />
+    <link href="images/favicon.png" rel="shortcut icon" type="image/x-icon" />
+
+
 @stop
 
 @section('content')
@@ -459,8 +470,8 @@
     <br />
     <br /> --}}
 
-    {{-- begin Chilo's design --}}
-    <div class="adbirt-content clearfix">
+    {{-- Begin Chilo's design --}}
+    {{-- <div class="adbirt-content clearfix">
         <div class="adbirt-page-heading custom-clearfix">
         </div><!-- adbirt-page-heading -->
 
@@ -696,7 +707,6 @@
             </ul>
         </div>
     </section>
-    {{-- End Chilo's Design --}}
 
     <br />
 
@@ -763,12 +773,175 @@
                 </p>
             </div>
         </div>
+    </div> --}}
+    {{-- End Chilo's Design --}}
+
+    {{-- Begin Reverted design --}}
+    <div class="header" id="home">
+        <div class="header_top">
+            <div class="wrap">
+                <div class="logo">
+                    <a href="/">
+
+                        <img src="{{ asset('images/logo.png') }}">
+
+
+                    </a>
+                </div>
+                <div class="menu">
+                    <ul>
+                        <li><a href="/" class="scroll">Home</a></li>
+                        <li><a href="howitworks" class="scroll">How it works</a></li>
+                        <li><a class="" href="pricing">Pricing</a></li>
+                        <!-- <li><a href="partners" class="scroll">Partners</a></li> -->
+                        <li><a href="support" class="scroll">Support</a></li>
+                        <li class="login">
+                            <div id="loginContainer"><a href="login" id="loginButton"><span>Login</span></a>
+
+                            </div>
+                        </li>
+                        <div class="clear"></div>
+                    </ul>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>
     </div>
+    <div class="main">
+        <div class="content">
+            <div class="content_top">
+                <div class="wrap">
+                    <div class="banner_desc">
+                        <h1><small class="set">Set the Cost!</small> <br /><span>Pay for Result!</span></h1>
+                        <h3> <small class="set2">You set your Budget (Cost Per Action) and<br /> only pay when
+                                your Product is consumed.</small></h3>
+                        <a class="play_icon fancybox-media" href="https://vimeo.com/33790882">
 
-@stop
+                            <!-- <img src="{{ asset('images/play-icon.png') }}"> -->
 
-@section('script')
-    <!-- Wavify JS -->
+                        </a>
+                        <h3>Signup for <span>FREE</span> today!</h3>
+                        <p>No credit card required.</p>
+                        <div class="sign_up">
+                            <form>
+                                <input type="text" value="E-mail address " onFocus="this.value = '';"
+                                    onBlur="if (this.value == '') {this.value = 'E-mail address';}">
+                                <input type="submit" value="Sign Up">
+                            </form>
+                        </div>
+                    </div>
+                    <div class="ipad">
+                        <!-- <img src="images/ipad.png" alt="" /> -->
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            </div>
+
+            <div class="features" id="features">
+                <div class="wrap">
+                    <h2>Adbirt for <span>Advertisers</span></h2>
+                    <h4>Signup as an Advertiser within minutes &amp; get your Ads across our Ad Networks</h4>
+                    <div class="features_grids">
+                        <div class="section group">
+                            <div class="grid_1_of_4 images_1_of_4">
+
+                                <img src="{{ asset('images/signup.jpg') }}">
+
+
+                                <h3>Create an Account</h3>
+                                <p>Signing up on Adbirt as an Advertiser is the first step to putting your message across
+                                    our Ad Network.</p>
+                            </div>
+                            <div class="grid_1_of_4 images_1_of_4">
+
+                                <img src="{{ asset('images/fundwallet.jpg') }}">
+
+
+                                <h3>Fund your Ads Wallet</h3>
+                                <p>You can Fund your Wallet with as low as $1 and you can set a CPA (Cost per action) to as
+                                    low as $0.01 / Action.</p>
+                            </div>
+                            <div class="grid_1_of_4 images_1_of_4">
+
+                                <img src="{{ asset('images/createads.jpg') }}">
+
+
+                                <h3>Create your First Ads</h3>
+                                <p>Create a nice and professional looking Ads to capture your Audience using our Ads
+                                    Campaign builder .</p>
+                            </div>
+                            <div class="grid_1_of_4 images_1_of_4">
+
+                                <img src="{{ asset('images/setbudget.jpg') }}">
+
+
+                                <h3>Set your Budget</h3>
+                                <p>Like we made mention earlier, you are in control of the CPA, set your Budget your way,
+                                    within your means.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="features" id="features">
+                <div class="wrap">
+                    <h2>Adbirt for <span class="pub">Publishers</span></h2>
+                    <h4>Signup as a Publisher within minutes &amp; start earning</h4>
+                    <div class="features_grids">
+                        <div class="section group">
+                            <div class="grid_1_of_4 images_1_of_4">
+
+                                <img src="{{ asset('images/publisher-signup.jpg') }}">
+
+
+                                <h3>Create an Account</h3>
+                                <p>Setup your Publishers Account by telling us more about your medium of Monetization.</p>
+                            </div>
+                            <div class="grid_1_of_4 images_1_of_4">
+
+                                <img src="{{ asset('images/choosead.jpg') }}">
+
+
+                                <h3>Choose an Ad Campaign</h3>
+                                <p>Pick a Campaign that is suitable to the interest of your Audience, be it Email list or
+                                    website.</p>
+                            </div>
+                            <div class="grid_1_of_4 images_1_of_4">
+
+                                <img src="{{ asset('images/placeitonyourwebsite.jpg') }}">
+
+
+                                <h3>Place it on your Website</h3>
+                                <p>Place it on your Website, Email list, Social media handles, WhatsApp group, Blog or other
+                                    channels.</p>
+                            </div>
+                            <div class="grid_1_of_4 images_1_of_4">
+
+                                <img
+                                    src="{{ asset('images/earn-and-withdraw-instantly-from-adbirt-publisher-account.jpg') }}">
+
+
+                                <h3>Earn and Withdraw Instantly</h3>
+                                <p>Track your earnings and make withdrawal request anytime you earn, No threshold, no limit
+                                    to earnings.</p>
+                            </div>
+                        </div>
+                        <div class="button"><a class="" href="/register">Signup Now
+                                <img src="{{ asset('images/arrow.png') }}">
+                            </a>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {{-- End Reverted design --}}
+
+        @stop
+
+        @section('script')
+            {{-- <!-- Wavify JS -->
     <script src="public/assets-revamp/js/wavify.js"></script>
     <script src="public/assets-revamp/js/TweenMax.min.js"></script>
     <!-- jQuery Wavify JS -->
@@ -788,5 +961,5 @@
             width: '100%',
             zIndex: 999
         })
-    </script>
-@stop
+    </script> --}}
+        @stop

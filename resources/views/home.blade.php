35623ddc3080c7dceab741f2c5f7cc2d58fec7a3
danroyal001 <danroyaleffiong@gmail.com>
2022-05-07 01:54:27 +0100

    _

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -135,7 +135,7 @@
 
         <div class="features" id="features">
             <div class="wrap">
-                <h2>Adbirt for <span class="text-primary-color">Advertisers</span></h2>
+                <h2>Adbirt for <span class="text-primary-color" style="font-size: 64px !important;">Advertisers</span></h2>
                 <h4>Signup as an Advertiser within minutes &amp; get your Ads across our Ad Networks</h4>
                 <div class="features_grids">
                     <div class="section group">

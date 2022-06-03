7edc1937646c009e87c865d6b61a3f36400d84d5
danroyal001 <danroyaleffiong@gmail.com>
2022-06-03 17:00:38 +0100

    _

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -229,89 +229,73 @@
                     </div>
                 </div>
 
-                <br />
-                <br />
-                <br />
-
-                <h3 style="font-size: 30px !important;" class="text-center w-100 times-roman-font">Get the best results that
-                    interest you</h3>
-
-                <br />
-                <br />
-                <br />
-
-                <div class="row owl-theme d-flex align-items-center justify-content-between p-0 m-0">
-
-                    <div class="col-12 col-md-4 col-lg-4 adbirt-best-service-slide m-0 p-0 wow zoomIn p-3"
-                        data-wow-duration="0.45s" data-wow-delay="0.4s" data-wow-offset="0">
-                        <div class="">
-                            <div class="adbirt-best-service-img d-flex flex-row justify-content-center">
-                                <img src="/public/assets-revamp/img/services/shopping-action.jpg" alt="CPA">
+                {{-- begin: Chilo --}}
+                <!-- FEATURES SECTION  -->
+                <section class="mb-5 mx-4 mx-md-5 px-md-5 pt-0">
+                    <div class="my-5">
+                        <div class="row mx-2 mx-sm-0 mx-md-5">
+                            <div class="col col-lg-4 col-md-6 col-12">
+                                <div class="feature_card d-flex flex-column align-items-center">
+                                    <div class="feature_iconWrap">
+                                        <img src="/public/assets-revamp/img/actions-and-events/double-click.png"
+                                            class="feature_icon" alt="click" />
+                                    </div>
+                                    <div class="my-4 d-flex flex-column align-items-center">
+                                        <p class="h4 mt-3 open-sans-text text-custom-color">Cost Per Action</p>
+                                        <p
+                                            class="mx-0 mx-lg-2 text-center open-sans-text font-size-16 text-custom-color font-weight-bold">
+                                            Promote your products or service online pay your desired amount when you make a
+                                            sale
+                                            or get a new lead to
+                                            your business.
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="adbirt-best-service-content p-2">
-                                <h3 class="adbirt-best-service-title">Cost Per Action</h3>
-                                <p
-                                    class="shadow-lg p-3 adbirt-best-service-height adbirt-best-service-description cursive-font">
-                                    Promote your
-                                    products or service online pay your
-                                    desired
-                                    amount when you make a sale or get a new lead to your business.
-                                </p>
+                            <div class="col col-lg-4 col-md-6 col-12">
+                                <div class="feature_card d-flex flex-column align-items-center">
+                                    <div class="feature_iconWrap">
+                                        <img src="/public/assets-revamp/img/actions-and-events/acquisition.png"
+                                            class="feature_icon" alt="click" />
+                                    </div>
+                                    <div class="my-4 d-flex flex-column align-items-center">
+                                        <p class="h4 mt-3 open-sans-text text-custom-color">Cost Per Sales</p>
+                                        <p
+                                            class="mx-0 mx-lg-2 text-center open-sans-text font-size-16 text-custom-color font-weight-bold">
+                                            You're in total control of your ad budget, set the cost within your means and
+                                            only
+                                            pay after a successful
+                                            sales or leads.
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-
-
-                    <div class="col-12 col-md-4 col-lg-4 adbirt-best-service-slide m-0 p-0 wow zoomIn p-3"
-                        data-wow-duration="0.45s" data-wow-delay="0.4s" data-wow-offset="0">
-                        <div class="">
-
-                            <div class="adbirt-best-service-img d-flex flex-row justify-content-center">
-                                <img src="/public/assets-revamp/img/services/cost-per-sale.png" alt="CPC">
-                            </div>
-                            <div class="adbirt-best-service-content p-2">
-                                <h3 class="adbirt-best-service-title">Cost per Sales</h3>
-                                <p
-                                    class="shadow-lg p-3 adbirt-best-service-height adbirt-best-service-description px-1 cursive-font">
-                                    You're
-                                    in total control of your ad budget, set the
-                                    cost
-                                    within your means and only pay after a successful sales or leads.
-                                </p>
-                            </div>
-
-                        </div>
-                    </div>
-
-
-                    <div class="col-12 col-md-4 col-lg-4 adbirt-best-service-slide m-0 p-0 wow zoomIn p-3"
-                        data-wow-duration="0.45s" data-wow-delay="0.4s" data-wow-offset="0">
-                        <div class="">
-                            <div class="adbirt-best-service-img d-flex flex-row justify-content-center">
-                                <img src="/public/assets-revamp/img/services/click-action.jpeg" alt="CPD">
-                            </div>
-                            <div class="adbirt-best-service-content p-2">
-                                <h3 class="adbirt-best-service-title">Cost Per Click</h3>
-                                <p
-                                    class="shadow-lg p-3 adbirt-best-service-height adbirt-best-service-description cursive-font">
-                                    Get unlimited
-                                    impressions to your business for free
-                                    and
-                                    only pay for a valid click when users visit or land on your websites.
-                                </p>
+                            <div class="col col-lg-4 col-md-6 col-12">
+                                <div class="feature_card d-flex flex-column align-items-center">
+                                    <div class="feature_iconWrap">
+                                        <img src="/public/assets-revamp/img/actions-and-events/click.png"
+                                            class="feature_icon" alt="click icon" />
+                                    </div>
+                                    <div class="my-4 d-flex flex-column align-items-center">
+                                        <p class="h4 mt-3 open-sans-text text-custom-color">Cost Per Click</p>
+                                        <p
+                                            class="mx-0 mx-lg-2 text-center open-sans-text font-size-16 text-custom-color font-weight-bold">
+                                            Get unlimited impressions to your business for free and only pay for a valid
+                                            click
+                                            when users visit or
+                                            land on your websites.
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
+                        <br />
+                        <div class="row d-flex justify-content-center">
+                            <a href="/dashboard" class="btn btn-danger bg-primary-color">Start Now</a>
+                        </div>
                     </div>
-
-                    <div class="d-flex flex-column align-items-center justify-content-center w-100">
-                        <a href="/dashboard" class="adbirt-themes-btn red mt-3">Start now for free</a>
-                    </div>
-                </div>
-
-                <br />
-                <br />
-                <br />
-                <!--- END ROW -->
+                </section>
+                {{-- end: Chilo --}}
 
             </div>
 

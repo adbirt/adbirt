fce66e5f844bc1dcdb27d9b530ee643a964c0b76
danroyal001 <danroyaleffiong@gmail.com>
2022-06-27 13:44:53 +0100

    _

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -79,7 +79,7 @@
                 {!! Form::close() !!}
 
                 <p class="loginhere">
-                    Already have an account?<a href="/login" class="loginhere-link"> Log in</a>
+                    Already have an account?<a href="/login" class="text-primary-color"> Log in</a>
                     <br />
                     Or, return to <a href="/" class="loginhere-link text-primary-color">home page</a>
                 </p>

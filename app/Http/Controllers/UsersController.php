5d45c2bce83bc28d732fd5c04c6400f5070d2671
danroyal001 <danroyaleffiong@gmail.com>
2022-06-29 18:16:48 +0100

    _

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -37,7 +37,6 @@ class UsersController extends Controller
      */
     public function create()
     {
-
         $countries_url = "/public/assets-revamp/new-auth/json/countries-states-cities.json";
 
         $curl = curl_init($countries_url);

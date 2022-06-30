93df7e9e384c302604f34f4ed6b3e487136b1815
danroyal001 <danroyaleffiong@gmail.com>
2022-06-30 17:56:31 +0100

    _

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -70,7 +70,7 @@
 
                 <div class="modal-footer">
 
-                    <button type="button" class="btn btn-danger btn-rounded" data-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-danger btn-rounded btn-block" data-dismiss="modal">Cancel</button>
 
                     {!! Form::submit('Submit', ['class' => 'btn btn-danger bg-primary-color btn-rounded']) !!}
 

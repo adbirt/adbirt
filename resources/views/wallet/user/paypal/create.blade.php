7023bea6ed6cc757fb53829c48e740d124fa33a8
danroyal001 <danroyaleffiong@gmail.com>
2022-07-05 12:09:33 +0100

    _

--- resources/views/wallet/user/paypal/create.blade.php
+++ resources/views/wallet/user/paypal/create.blade.php
@@ -22,7 +22,7 @@
             </div>
             <ol class="breadcrumb">
                 <!--<li><a href="#">Home</a></li>
-                                                    <li class="active">Paypal Deposit</li>-->
+                                                        <li class="active">Paypal Deposit</li>-->
             </ol>
             <div class="card">
                 <ul class="nav nav-tabs">
@@ -30,8 +30,8 @@
                         <a class="nav-link active" href="#first" data-toggle="tab">{!! $title !!}</a>
                     </li>
                     <!-- <li class="nav-item">
-                                                            <a class="nav-link" href="#second" data-toggle="tab">Billing</a>
-                                                          </li> -->
+                                                                <a class="nav-link" href="#second" data-toggle="tab">Billing</a>
+                                                              </li> -->
                 </ul>
                 <div class="p-a-2 tab-content">
                     <div class="tab-pane active p-3" id="first">

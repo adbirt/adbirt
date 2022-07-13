6a59a2280ef842f59e4f189d60c82651cd268086
danroyal001 <danroyaleffiong@gmail.com>
2022-07-13 17:15:04 +0100

    _

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
     <link href="{{ asset('/css/style.css') }}" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,700,500,900' rel='stylesheet' type='text/css'>
     <link href="{{ asset('/css/magnific-popup.css') }}" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="/css/jquery.fancybox.css" media="screen" />
+    {{-- <link rel="stylesheet" type="text/css" href="/css/jquery.fancybox.css" media="screen" /> --}}
 @stop
 
 

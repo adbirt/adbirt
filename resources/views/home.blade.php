08de3bc74a3ca33fa5ec64f947538f8de57fc271
danroyal001 <danroyaleffiong@gmail.com>
2022-07-13 17:46:42 +0100

    _

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.home_layout')
 {{-- @extends('layouts.frontend') --}}
 
-
 @section('style')
     <link href="{{ asset('/css/style.css') }}" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,700,500,900' rel='stylesheet' type='text/css'>
@@ -9,9 +8,7 @@
     {{-- <link rel="stylesheet" type="text/css" href="/css/jquery.fancybox.css" media="screen" /> --}}
 @stop
 
-
 @section('content')
-
     {{-- Begin: Reverted header --}}
     <div class="header" id="home">
         <div class="header_top">

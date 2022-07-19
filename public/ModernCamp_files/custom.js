952f30c494daa0cda7efb955331fd753e9d7a5fd
danroyal001 <danroyaleffiong@gmail.com>
2022-07-19 14:38:48 +0100

    _

--- public/ModernCamp_files/custom.js
+++ public/ModernCamp_files/custom.js
@@ -38,18 +38,6 @@
         });
     }
 
-	//disable right click
-	window.oncontextmenu = function () {
-			return false;
-		}
-		$(document).keydown(function (event) {
-			if (event.keyCode == 123) {
-				return false;
-			}
-			else if ((event.ctrlKey && event.shiftKey && event.keyCode == 73) || (event.ctrlKey && event.shiftKey && event.keyCode == 74)) {
-				return false;
-			}
-		});
     /*
      * ----------------------------------------------------------------------------------------
      *  PRELOADER JS
